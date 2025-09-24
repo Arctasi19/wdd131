d81628088083ae707d868a37358c51a9f7e39f9c
Arctasi19 <joel.young04@gmail.com>
2025-09-24 15:00:55 +0200

    Dom Basics assignment W2

---
+++ mission/week2/dom-basics.js
@@ -0,0 +1,13 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+const newImage = document.createElement("img");
+newImage.src = "https://picsum.photos/200";
+newImage.alt = "provided image";
+document.body.appendChild(newImage);
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+const newSection = document.createElement("Section");
+newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+document.body.appendChild(newSection);
